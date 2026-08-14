bede2266ee9748fe11ae6774875331a7409e6877
HappyH <stu246576@mail.uni-kiel.de>
2026-08-14 14:05:11 +0200

    fix broken link

--- better-univis.user.js
+++ better-univis.user.js
@@ -6,7 +6,7 @@
 // @version     1.1.1
 // @author      HappyH
 // @description Verbessert UnivIS
-// @require     https://unpkg.com/darkreader@4.9.105/darkreader.js#sha512=2b7f8f0cb074b0f1ca650f8feb81e345232a9f973257481dc0f56e8fcabb44f052e591f9039b684490c4e650bb71024f365fa085539a4213ad21bd7f15d28e93
+// @require     https://unpkg.com/darkreader
 // @run-at      document-body
 // ==/UserScript==
 
